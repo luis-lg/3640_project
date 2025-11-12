f74c8f518b7b3c89eeeeb10637c42a23d5c0ab8d
Bryan Rodriguez <bryanrodrig28@Bryans-MacBook-Air-7.local>
2025-11-11 22:33:39 -0800

    Included how-to comments to run app

--- src/main/java/com/chatapp/Application.java
+++ src/main/java/com/chatapp/Application.java
@@ -1,3 +1,13 @@
+/*  How to compile: 
+ * ./mvnw clean compile 
+ * 
+ * How to run: 
+ * 1. Navigate to the project directory (cd 3640_project)
+ * 2. Run " mvn spring-boot:run " to start the application	
+ * 3. Open a web browser and go to " http://localhost:8080/test-chat.html " to test the chat functionality
+ * 4. Open multiple browser tabs to simulate different users and test real-time messaging
+ */
+
 package com.chatapp;
 
 import org.springframework.boot.SpringApplication;
